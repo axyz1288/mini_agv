d25b7fd4ffaaec1c38ac4c44c3981ed1caebc697
Eric_Chang <axyz1350@gmail.com>
2021-01-21 22:58:44 +0800

    Fix: entrypoint do not git clone

--- k8s/docker/car_entrypoint.sh
+++ k8s/docker/car_entrypoint.sh
@@ -5,11 +5,6 @@ then
 	cp /etc/skel/.bashrc ~/
 	echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 fi
-if [ -f "~/car" ]
-then
-	rm -rf ~/car
-fi
 source /opt/ros/$ROS_DISTRO/setup.bash
-git clone https://github.com/axyz1288/car.git ~/car
 cd ~/car && catkin_make
 exec "$@"
