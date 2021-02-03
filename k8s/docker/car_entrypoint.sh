141ea8d4c240864add8125b73ec561f2928d9b30
Eric_Chang <axyz1350@gmail.com>
2021-02-04 00:41:18 +0800

    K8s: fix dockerfile and entrypoint display

--- k8s/docker/car_entrypoint.sh
+++ k8s/docker/car_entrypoint.sh
@@ -9,5 +9,5 @@ source /opt/ros/$ROS_DISTRO/setup.bash
 cd ~/slamware_ros_sdk_linux-aarch64-gcc5.4 && catkin_make && source ./devel/setup.bash
 cd ~/car && catkin_make
 
-~/car/build/car $1 $2 &
-roslaunch slamware_ros_sdk slamware_ros_sdk_server_node.launch
+roslaunch slamware_ros_sdk slamware_ros_sdk_server_node.launch ip_address:=192.168.11.1 &
+exec "$@"
