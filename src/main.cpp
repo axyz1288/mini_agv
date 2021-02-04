576fe2a6dcf9b2a8639b591e658d87f62448524b
Eric_Chang <axyz1350@gmail.com>
2021-02-04 13:25:37 +0800

    Cpp: fix main.cpp type to GetAction

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,17 @@ int main(int argc, char *argv[])
     while(ros::ok)
     {
         agv->CheckData();
-        if(type == 0 || type == 5)
+        if(agv->GetAction() == 0 || agv->GetAction() == 5)
             agv->Stop();
-        else if(type == 1)
+        else if(agv->GetAction() == 1)
             thread* forward = new thread(&AGV::MoveForward, agv, 0.5f, 100);
-        else if(type == 2)
+        else if(agv->GetAction() == 2)
             thread* backward = new thread(&AGV::MoveBackward, agv, 0.5f, 100);
-        else if(type == 3)
+        else if(agv->GetAction() == 3)
             thread* left = new thread(&AGV::RotateLeft, agv, M_PI_2, 100);
-        else if(type == 4)
+        else if(agv->GetAction() == 4)
             thread* right = new thread(&AGV::RotateRight, agv, M_PI_2, 100);
-        else if(type == 6)
+        else if(agv->GetAction() == 6)
             agv->Put();
         else
         {
