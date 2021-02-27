a96c8270b091ce4eb6fd5dcfb1ceed2b8567ddcf
Eric_Chang <axyz1350@gmail.com>
2021-02-28 03:49:38 +0800

    Dockerfile: change git clone method

--- k8s/docker/mini_agv_entrypoint.sh
+++ k8s/docker/mini_agv_entrypoint.sh
@@ -6,6 +6,11 @@ then
 	echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 fi
 source /opt/ros/$ROS_DISTRO/setup.bash
+
+git clone https://github.com/axyz1288/mini_agv.git ~/mini_agv
+tar -C ~ -Jxf ~/mini_agv/slam.tar.xz
+chmod 777 -R /home/${USERNAME}/mini_agv
+chmod 777 -R /home/${USERNAME}/slam
 cd ~/slam && catkin_make && source ./devel/setup.bash
 cd ~/mini_agv && catkin_make
 
