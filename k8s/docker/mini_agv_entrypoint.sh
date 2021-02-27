622a32ee30f37eaa58b4bcb1200c656572c057f0
Eric_Chang <axyz1350@gmail.com>
2021-02-28 02:46:02 +0800

    Update: auto download mini_agv

--- k8s/docker/mini_agv_entrypoint.sh
+++ k8s/docker/mini_agv_entrypoint.sh
@@ -6,7 +6,7 @@ then
 	echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 fi
 source /opt/ros/$ROS_DISTRO/setup.bash
-cd ~/slamware_ros_sdk_linux-aarch64-gcc5.4 && catkin_make && source ./devel/setup.bash
+cd ~/slam && catkin_make && source ./devel/setup.bash
 cd ~/mini_agv && catkin_make
 
 roslaunch slamware_ros_sdk slamware_ros_sdk_server_node.launch ip_address:=192.168.11.1 &
