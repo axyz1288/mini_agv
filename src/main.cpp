3a02acb2a41e6525205023f915e7a2d88208e9ea
Eric_Chang <axyz1350@gmail.com>
2021-02-28 21:29:57 +0800

    Cpp: ip2NodeName

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,29 @@
 #include "./AGV/AGV.h"
 #include <ros/ros.h>
 
+const string Ip2NodeName(const string ip)
+{
+    if(ip == "192.168.50.249")
+        return "agv0";
+    else if(ip == "192.168.50.20")
+        return "agv1";
+    else if(ip == "192.168.50.55")
+        return "agv2";
+    else if(ip == "192.168.50.")
+        return "agv3";
+    else if(ip == "192.168.50.")
+        return "agv4";
+    else if(ip == "192.168.50.")
+        return "agv5";
+    else
+        return "0";
+}
+
 int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, argv[1]);
-    AGV *agv = AGV::getAGV(argv[1], argv[2]);
+    const string nodeName = Ip2NodeName(argv[1]);
+    ros::init(argc, argv, nodeName);
+    AGV *agv = AGV::getAGV(nodeName, argv[2]);
     while(ros::ok)
     {
         agv->CheckData();
@@ -15,9 +34,9 @@ int main(int argc, char *argv[])
         else if(agv->GetAction() == 2)
             thread* backward = new thread(&AGV::MoveBackward, agv, 0.5f, 100);
         else if(agv->GetAction() == 3)
-            thread* left = new thread(&AGV::RotateLeft, agv, M_PI_2, 100);
+            thread* left = new thread(&AGV::MoveLeft, agv, 0.5f, 100);
         else if(agv->GetAction() == 4)
-            thread* right = new thread(&AGV::RotateRight, agv, M_PI_2, 100);
+            thread* right = new thread(&AGV::MoveRight, agv, 0.5f, 100);
         else if(agv->GetAction() == 6)
             agv->Put();
         else
